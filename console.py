cf1f263f73d3ea529d84a05435f1ed59c91d3aaa
Virteip <sergiopietri@gmail.com>
2020-02-18 18:25:24 -0500

    [#7 create and destroy, anihilate, obliterate]

--- console.py
+++ console.py
@@ -18,18 +18,62 @@ class HBNBCommand(cmd.Cmd):
         """
         pass
 
-    def do_quit(self, arg):
+    def do_quit(self, args):
         """
         Exit the program
         """
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, args):
         """
         Exit the program
         """
         print()
         return True
+
+    def do_create(self, args):
+        """
+        Creates a new instance of BaseModel,
+        saves it (to the JSON file) and prints the id
+        """
+        if len(args) is not 0:
+            try:
+                my_model = eval(arg + "()")
+                my_model.save()
+                print(my_model.id)
+            except:
+                print("** class doesn't exist **")
+        if len(args) is 0:
+            print("** class name missing **")
+            return None
+
+    def do_destroy(self, args):
+        """
+        Deletes an instance based on the class name and id
+        """
+        args = args.split()
+
+        if len(args) is 0:
+            print("** class name missing **")
+            return None
+        if len(args) is not 0:
+            try:
+                eval(args[0])
+            except:
+                print("** class doesn't exist **")
+                return None
+        if len(args) < 2:
+            print("** instance id missing **")
+            return None
+
+        keys = args[0] + "." + args[1].replace("\"", "")
+
+        if keys in storage.all():
+            del storage.all()[keys]
+            storage.save()
+        else:
+            print("** no instance found **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
