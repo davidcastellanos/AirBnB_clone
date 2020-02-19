a6667278e4185909b7f4cb37f5f45b70976ae41a
Virteip <sergiopietri@gmail.com>
2020-02-18 22:41:31 -0500

    [#7 create, all; show, not working, destroy not sure, missing update ]

--- console.py
+++ console.py
@@ -3,8 +3,8 @@
 Console prompt hbnb
 """
 import cmd
-
-
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -36,43 +36,80 @@ class HBNBCommand(cmd.Cmd):
         Creates a new instance of BaseModel,
         saves it (to the JSON file) and prints the id
         """
-        if len(args) is not 0:
-            try:
-                my_model = eval(arg + "()")
-                my_model.save()
-                print(my_model.id)
-            except:
-                print("** class doesn't exist **")
         if len(args) is 0:
             print("** class name missing **")
             return None
+        try:
+            my_model = eval(args + "()")
+            my_model.save()
+            print(my_model.id)
+        except:
+            print("** class doesn't exist **")
 
-    def do_destroy(self, args):
-        """
-        Deletes an instance based on the class name and id
-        """
-        args = args.split()
+    # def do_show(self, args):
+    #     """
+    #     Prints the string representation of an instance
+    #     based on the class name and id
+    #     """
+    #     split = args.split()
+    #     if len(args) is 0:
+    #         print("** class name missing **")
+    #         return None
+    #     elif len(args) is 1:
+    #         print("** instance id missing **")
+    #         return None
+    #     args = args.split()
+    #     keys = args[0] + "." + args[1].replace("\"", "")
+    #     if keys in storage.all():
+    #         print(storage.all()[keys])
+    #     else:
+    #         print("** no instance found **")
 
-        if len(args) is 0:
-            print("** class name missing **")
-            return None
-        if len(args) is not 0:
+
+    # def do_destroy(self, args):
+    #     """
+    #     Deletes an instance based on the class name and id
+    #     """
+    #     args = args.split()
+
+    #     if len(args) is 0:
+    #         print("** class name missing **")
+    #         return None
+    #     elif len(args) is not 0:
+    #         try:
+    #             eval(args[0])
+    #         except:
+    #             print("** class doesn't exist **")
+    #             return None
+    #     elif len(args) < 2:
+    #         print("** instance id missing **")
+    #         return None
+    #     else:
+    #         keys = args[0] + "." + args[1].replace("\"", "")
+    #         if keys in storage.all():
+    #             del storage.all()[keys]
+    #             storage.save()
+    #         else:
+    #             print("** no instance found **")
+
+    def do_all(self, args):
+        """
+        Display contents
+        """
+        split = args.split()
+        if split :
             try:
-                eval(args[0])
+                eval(split[0])
             except:
                 print("** class doesn't exist **")
                 return None
-        if len(args) < 2:
-            print("** instance id missing **")
-            return None
-
-        keys = args[0] + "." + args[1].replace("\"", "")
-
-        if keys in storage.all():
-            del storage.all()[keys]
-            storage.save()
-        else:
-            print("** no instance found **")
+        for key, value in storage.all().items():
+            if not split:
+                print(value)
+            else:
+                key = key.split('.')
+                if split[0] == key[0]:
+                    print(value)
 
 
 if __name__ == '__main__':
