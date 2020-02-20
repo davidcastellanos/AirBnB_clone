303602f2393292d37b9b570f8a6005d88f120154
davidcastellanos <davidcastellanos@users.noreply.github.com>
2020-02-19 21:43:19 -0500

    Feature david (#10)
    * [add models folder with init and base; testbasemodel]
    
    * [Task #3 complete]
    
    * [Task #4 full output need to correct datetime]
    
    * [Task #4 complete (I think) correct class output]
    
    * [Update] Added task 5
    
    * [#6 done]
    
    * [#7 create and destroy, anihilate, obliterate]
    
    * [#7 create, all; show, not working, destroy not sure, missing update ]
    
    * [Fixed Reload on file_storage, Fixed INIT on base model, Fixed Create, show, and destroy on console. Missing Update and All on console]
    
    * [Added All to console]
    
    * [Up to #7 checked pep8]
    
    * [Added comments to file_storage]
    
    * [Update] Added new directories and files
    
    * [Added 8 & 9]
    
    * [Added 8 & 9 and console]
    
    * [Pep8 console and place]
    
    * [Update] Added test file
    
    * [Pep8 revision]
    
    * [Pep8 revision console]
    
    * [chmoding engine files]
    
    * [checking comments]
    
    * [Update] Adden coments in files
    
    * [Update] Added new fixes in unitest
    
    Co-authored-by: Sergio Pietri <37060772+Virteip@users.noreply.github.com>

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -22,11 +22,19 @@ class TestBMDocs(unittest.TestCase):
 
     def test_pep8(self):
         """Test pep8"""
-         for path in ['models/base_model.py',
+        for path in ['models/base_model.py',
                      'tests/test_models/test_base_model.py']:
             with self.subTest(path=path):
                 errors = pycodestyle.Checker(path).check_all()
                 self.assertEqual(errors, 0)
+
+
+    def test_ModDocst(self):
+        """Test module DocString"""
+        self.assertIsNot(module_doc, None,
+                         "base_model.py needs the docStr")
+        self.assertTrue(len(module_doc) > 1,
+                        "base_model.py needs the docStr")
 
 
     def test_ModDocst(self):
@@ -41,3 +49,24 @@ class TestBMDocs(unittest.TestCase):
     #def test_classDocst(self):
         #"""Test Class DocString"""
 
+
+    def test_classDocst(self):
+        """Test Class DocString"""
+        self.assertIsNot(BaseModel.__doc__, None,
+                         "BaseModel class needs docStr")
+        self.assertTrue(len(BaseModel.__doc__) >= 1,
+                        "BaseModel class needs docStr")
+     
+     def test_FuncDocStr(self):
+        """Test for presence of docstr in BaseModel methods"""
+        for functions in self.BaseModel:
+            with self.subTest(function=functions):
+                self.assertIsNot(
+                    functions[1].__doc__,
+                    None,
+                    "{:s} method needs a docstring".format(functions[0])
+                )
+                self.assertTrue(
+                    len(functions[1].__doc__) > 1,
+                    "{:s} method needs a docstring".format(functions[0])
+                )
