4c56e6d901372a75a63c7753e04066eb60b51704
davidcastellanos <davidcastellanos@users.noreply.github.com>
2020-02-19 23:55:45 -0500

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -37,36 +37,11 @@ class TestBMDocs(unittest.TestCase):
                         "base_model.py needs the docStr")
 
 
-    def test_ModDocst(self):
-        """Test module DocString"""
-        self.assertIsNot(module_doc, None,
-                         "base_model.py needs a docstring")
-        self.assertTrue(len(module_doc) > 1,
-                        "base_model.py needs a docstring")
-
-    #def test_ModDocst(self):
-        #"""Test module DocString"""
-    #def test_classDocst(self):
-        #"""Test Class DocString"""
-
-
+   
     def test_classDocst(self):
         """Test Class DocString"""
         self.assertIsNot(BaseModel.__doc__, None,
                          "BaseModel class needs docStr")
         self.assertTrue(len(BaseModel.__doc__) >= 1,
                         "BaseModel class needs docStr")
-     
-     def test_FuncDocStr(self):
-        """Test for presence of docstr in BaseModel methods"""
-        for functions in self.BaseModel:
-            with self.subTest(function=functions):
-                self.assertIsNot(
-                    functions[1].__doc__,
-                    None,
-                    "{:s} method needs a docstring".format(functions[0])
-                )
-                self.assertTrue(
-                    len(functions[1].__doc__) > 1,
-                    "{:s} method needs a docstring".format(functions[0])
-                )
+   
