e7687dd739a66e3d79c0b8c4dc8ba5cad07880e4
davidcastellanos <castellanos.johann@gmail.com>
2020-02-19 21:39:58 -0500

    [Update] Added new fixes in unitest

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -22,7 +22,7 @@ class TestBMDocs(unittest.TestCase):
 
     def test_pep8(self):
         """Test pep8"""
-         for path in ['models/base_model.py',
+        for path in ['models/base_model.py',
                      'tests/test_models/test_base_model.py']:
             with self.subTest(path=path):
                 errors = pycodestyle.Checker(path).check_all()
@@ -31,10 +31,27 @@ class TestBMDocs(unittest.TestCase):
     def test_ModDocst(self):
         """Test module DocString"""
         self.assertIsNot(module_doc, None,
-                         "base_model.py needs a docstring")
+                         "base_model.py needs the docStr")
         self.assertTrue(len(module_doc) > 1,
-                        "base_model.py needs a docstring")
+                        "base_model.py needs the docStr")
 
-    #def test_classDocst(self):
-        #"""Test Class DocString"""
-
+    def test_classDocst(self):
+        """Test Class DocString"""
+        self.assertIsNot(BaseModel.__doc__, None,
+                         "BaseModel class needs docStr")
+        self.assertTrue(len(BaseModel.__doc__) >= 1,
+                        "BaseModel class needs docStr")
+     
+     def test_FuncDocStr(self):
+        """Test for presence of docstr in BaseModel methods"""
+        for functions in self.BaseModel:
+            with self.subTest(function=functions):
+                self.assertIsNot(
+                    functions[1].__doc__,
+                    None,
+                    "{:s} method needs a docstring".format(functions[0])
+                )
+                self.assertTrue(
+                    len(functions[1].__doc__) > 1,
+                    "{:s} method needs a docstring".format(functions[0])
+                )
